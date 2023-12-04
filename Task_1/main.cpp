4c7559dc1ba84098cabd06f56e21cb58a172b288
D4nilka Polekhin <polekhindaniil@yandex.ru>
2023-12-04 23:37:32 +0300

    Русский шифр

---
+++ Task_1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "/home/stud/C++Projects/Pr_TiMP/LB_TiMP/modAlphaCipher.h"
+using namespace std;
+
+void check(const wstring& Text, const wstring& key)
+{
+    wstring cipherText;
+    wstring decryptedText;
+    modAlphaCipher cipher(key);
+    cipherText = cipher.encrypt(Text);
+    decryptedText = cipher.decrypt(cipherText);
+    wcout<<L"key=" << key <<endl;
+    wcout<<Text<<endl;
+    wcout<<cipherText<<endl;
+    wcout<<decryptedText<<endl;
+    if (Text == decryptedText)
+        wcout<<L"ok\n";
+    else
+        wcout<<L"err\n";
+}
+
+int main(int argc, char **argv)
+{
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    check(L"ПЕРВЫХ", L"ВРЕМЯ");
+    return 0;
+}
