a1c2322c09e7ad91bec12c52c2e8295cd929cd3d
D4nilka Polekhin <polekhindaniil@yandex.ru>
2023-12-16 20:16:21 +0300

    Замена латинского сдвига

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include "/home/stud/C++Projects/Pr_TiMP/Lb1.2_TiMP/cifr.h"
+#include "/home/stud/C++Projects/Lb_TiMP/Lb_TiMP/Chifr.h"
 using namespace std;
 
 string encrypt(string in, const int kluch);
@@ -9,13 +9,14 @@ string decrypt(string in, const int kluch);
 
 int main(int argc, char **argv)
 {
-    cifr enc;
+    chifr enc;
     int COLS = 10;
 
-    string public_message = "BINDINGOFISAAC";
+    string public_message = "GERALTISRIVII";
     string privat_message = enc.encrypt(public_message, COLS);
     string decode_message = enc.decrypt(privat_message, COLS);
 
+    cout << "Столбцы = " << COLS << endl;
     cout << public_message << endl;
     cout << privat_message << endl;
     cout << decode_message << endl;
